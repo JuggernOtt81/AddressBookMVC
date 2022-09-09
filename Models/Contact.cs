e7db448df949aefdcc37ea6fcda34474eb9347b4
Lawson Ott <34386218+JuggernOtt81@users.noreply.github.com>
2022-09-09 14:55:13 -0400

    Add project files.

---
+++ Models/Contact.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AddressBookMVC.Models
+{
+    public class Contact
+    {
+        [Required]
+        [Display(Name ="First Name")]
+        public string FirstName { get; set; }
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [Display(Name = "Address")]
+        public string Address1 { get; set; }
+        [Display(Name = "Addr. line 2")]
+        public string Address2 { get; set; }
+        [Display(Name = "City")]
+        public string City { get; set; }
+        [Display(Name = "State")]
+        public string State { get; set; }
+        [Display(Name = "ZIP code (or Postal Code)")]
+        [DataType(DataType.PostalCode)]
+        public string PostZip { get; set; }
+        [Display(Name = "Email Address")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [Display(Name = "Phone Number")]
+        [DataType(DataType.PhoneNumber)]
+        public string Phone { get; set; }
+        public DateTime Created { get; set; }
+        [NotMapped]
+        [DataType(DataType.Upload)]
+        public IFormFile ImageFile { get; set; }
+        public byte[] ImageData { get; set; }
+        [Display(Name = ".jpg .png .gif .pdf")]
+        public string ImageType { get; set; }
+        [Display(Name = "Record Number: ")]
+        public int Id { get; set; }
+        [NotMapped]
+        public string FullName { get { return $"{FirstName} {LastName}"; } }
+    }
+}
